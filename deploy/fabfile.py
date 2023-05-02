a9bcea4058fbcbc7ec84cc4150e0101513186cc8
Kezhi Xiong <august.xkz@gmail.com>
2023-05-02 13:59:16 -0400

    Update fabfile.

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,14 +1,19 @@
 import os
 
 from fabric import task, Connection
-from invoke import UnexpectedExit
+from invoke import UnexpectedExit, run
 
 PASSWORD = os.environ['KHOURY_PASSWORD']
 SSH_KEY_PATH = '~/.ssh/id_rsa'
-ETCD_DIR = '~/etcd'
-SERVER_DIR = ETCD_DIR + '/server'
+
+REMOTE_ETCD_DIR = '~/etcd'
+REMOTE_SERVER_DIR = REMOTE_ETCD_DIR + '/server'
+
+LOCAL_ETCD_DIR = '~/go/src/go.etcd.io/etcd'
+LOCAL_SERVER_DIR = LOCAL_ETCD_DIR + '/server'
 
 HTTP_SCHEME = 'http://'
+LOCAL_HOST = '127.0.0.1'
 
 
 class EtcdConfig:
@@ -16,34 +21,49 @@ class EtcdConfig:
     port: int
     name: str
 
+    TickMs: int = 10
+    ElectionMs: int = 100
+
     listenClientUrls: str
     advertiseClientUrls: str
 
     initialAdvertisePeerUrls: str
     listenPeerUrls: str
     initialCluster: str
+    initialClusterState: str = "new"
 
     preVote: str = "false"
-    logLevel: str = "debug"
+
+    logLevel: str
+
+    def join(self):
+        self.initialClusterState = "existing"
 
     def __str__(self):
         return '--data-dir=' + 'data.etcd.' + self.name \
             + ' --name=' + self.name \
+            + ' --heartbeat-interval=' + str(self.TickMs) \
+            + ' --election-timeout=' + str(self.ElectionMs) \
             + ' --listen-client-urls=' + self.listenClientUrls \
             + ' --advertise-client-urls=' + self.advertiseClientUrls \
             + ' --initial-advertise-peer-urls=' + self.initialAdvertisePeerUrls \
             + ' --listen-peer-urls=' + self.listenPeerUrls \
             + ' --initial-cluster=' + self.initialCluster \
+            + ' --initial-cluster-state=' + self.initialClusterState \
             + ' --pre-vote=' + self.preVote \
             + ' --log-level=' + self.logLevel
 
 
-def parse_configs(cluster_url: str) -> dict:
+def parse_configs(cluster_url: str, merge: bool = False, logging: str = 'debug') -> list:
     if len(cluster_url) == 0:
         print("empty cluster url")
         exit(1)
 
-    configs = dict()
+    if logging not in ['debug', 'info', 'warn', 'panic', 'fatal']:
+        print("invalid logging level")
+        exit(1)
+
+    configs = []
     for node in cluster_url.split(","):
         tokens = node.split("=")
         name = tokens[0]
@@ -55,34 +75,58 @@ def parse_configs(cluster_url: str) -> dict:
 
         config = EtcdConfig()
         config.ip = ip
-        config.port= port
+        config.port = port
         config.name = name
 
+        if merge:
+            config.TickMs = 10
+            config.ElectionMs = 100
+
         # client comm
-        config.listenClientUrls = HTTP_SCHEME + ip + ':' + str(port-1)
-        config.advertiseClientUrls = HTTP_SCHEME + ip + ':' + str(port-1)
+        config.listenClientUrls = HTTP_SCHEME + ip + ':' + str(port - 1)
+        config.advertiseClientUrls = HTTP_SCHEME + ip + ':' + str(port - 1)
 
         # peer comm
         config.initialAdvertisePeerUrls = url
         config.listenPeerUrls = url
         config.initialCluster = cluster_url
 
-        configs[ip] = config
+        config.logLevel = logging
+
+        configs.append(config)
 
     return configs
 
 
 @task
-def start(ctx, cluster_url):
+def start(ctx, cluster_url, merge='false', logging='debug'):
     """
     cluster_url format: 1=http://127.0.0.1:1380,2=http://127.0.0.1:2380,3=http://127.0.0.1:3380,4=http://127.0.0.1:4380,5=http://127.0.0.1:5380,6=http://127.0.0.1:6380
     """
-    configs = parse_configs(cluster_url)
-    for ip in configs:
-        cfg = configs[ip]
-        run_cmd(ip, 'cd {} && '.format(SERVER_DIR) +
-                'nohup {} > etcd.{}.out 2>&1 &'.format('./server ' + str(cfg), cfg.name))
-        print("started " + ip)
+    merge = merge.lower() == 'true'
+    configs = parse_configs(cluster_url, merge, logging)
+    for cfg in configs:
+        start_config(cfg)
+
+
+@task
+def start_join(ctx, cluster_url, names, logging='debug'):
+    """
+    cluster_url format: 1=http://127.0.0.1:1380,2=http://127.0.0.1:2380,3=http://127.0.0.1:3380,4=http://127.0.0.1:4380,5=http://127.0.0.1:5380,6=http://127.0.0.1:6380
+    """
+    configs = parse_configs(cluster_url, False, logging)
+    names = set(names.split(','))
+    for cfg in configs:
+        if cfg.name in names:
+            cfg.join()
+            start_config(cfg)
+
+
+def start_config(cfg: EtcdConfig):
+    ip = cfg.ip
+    run_cmd(ip, 'cd {} && '.format(LOCAL_SERVER_DIR if ip == LOCAL_HOST else REMOTE_SERVER_DIR) +
+            'nohup {} > etcd.{}.out 2>&1 &'.format('./server ' + str(cfg), cfg.name))
+    print("started on " + ip)
 
 
 @task
@@ -91,10 +135,10 @@ def stop(ctx, cluster_url):
     cluster_url format: 1=http://127.0.0.1:1380,2=http://127.0.0.1:2380,3=http://127.0.0.1:3380,4=http://127.0.0.1:4380,5=http://127.0.0.1:5380,6=http://127.0.0.1:6380
     """
     configs = parse_configs(cluster_url)
-    for ip in configs:
-        cfg = configs[ip]
-        run_cmd(cfg.ip, 'kill -9 $(/usr/sbin/lsof -t -i:{})'.format(cfg.port))
-        print("stopped " + ip)
+    for cfg in configs:
+        ip = cfg.ip
+        run_cmd(cfg.ip, 'kill -9 $({}lsof -t -i:{})'.format('' if ip == LOCAL_HOST else '/usr/sbin/', cfg.port))
+        print("stopped on " + ip)
 
 
 @task
@@ -103,11 +147,11 @@ def clean(ctx, cluster_url):
     cluster_url format: 1=http://127.0.0.1:1380,2=http://127.0.0.1:2380,3=http://127.0.0.1:3380,4=http://127.0.0.1:4380,5=http://127.0.0.1:5380,6=http://127.0.0.1:6380
     """
     configs = parse_configs(cluster_url)
-    for ip in configs:
-        cfg = configs[ip]
-        run_cmd(cfg.ip, 'cd {} && '.format(SERVER_DIR) +
+    for cfg in configs:
+        ip = cfg.ip
+        run_cmd(ip, 'cd {} && '.format(LOCAL_SERVER_DIR if ip == LOCAL_HOST else REMOTE_SERVER_DIR) +
                 'rm -rf data.etcd.* && rm etcd.*.out')
-        print("cleaned " + ip)
+        print("cleaned on " + ip)
 
 
 @task
@@ -115,12 +159,15 @@ def logs(ctx, ip):
     """
     cluster_url format: 1=http://127.0.0.1:1380,2=http://127.0.0.1:2380,3=http://127.0.0.1:3380,4=http://127.0.0.1:4380,5=http://127.0.0.1:5380,6=http://127.0.0.1:6380
     """
-    os.system('scp -i {} {}:{}/etcd.*.out .'.format(SSH_KEY_PATH, ip, SERVER_DIR))
+    os.system('scp {}:{}/etcd.*.out .'.format(ip, REMOTE_SERVER_DIR))
 
 
 def run_cmd(ip, cmd):
-    with Connection(host=ip, connect_kwargs={'password': PASSWORD}) as conn:
-        try:
-            conn.run(cmd, asynchronous=True)
-        except UnexpectedExit:
-            print("run cmd failed: " + cmd)
+    if ip == LOCAL_HOST:
+        run(cmd, asynchronous=True)
+    else:
+        with Connection(host=ip, connect_kwargs={'password': PASSWORD}) as conn:
+            try:
+                conn.run(cmd, asynchronous=True)
+            except UnexpectedExit:
+                print("run cmd failed: " + cmd)
