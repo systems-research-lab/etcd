a3bc056fe28f821d123f6328bb38e85a89264b87
Ubuntu <jhankyulkang@gmail.com>
2023-12-12 04:15:18 +0000

    ignore testing

--- etcdctl/put.sh
+++ etcdctl/put.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 start=`date +%s%N`
-./etcdctl --endpoints=192.168.0.36:2380,192.168.0.239:2380,192.168.0.128:2380 put key value
+./etcdctl --endpoints=192.168.0.36:2380,192.168.0.239:2380,192.168.0.128:2380 get key value
 end=`date +%s%N`
 echo Execution time was `expr $end - $start` nanoseconds.
